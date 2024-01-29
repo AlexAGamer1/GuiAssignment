ec135f6b6cba79dbf446168a455b28f529d0ce1c
AlexAGamer1 <158062803+AlexAGamer1@users.noreply.github.com>
2024-01-29 14:43:32 +0000

    S

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.awt.event.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+//Change
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
